c47ade440ff8b33e947aba1bdff74e36502d2e24
Pruno <luca.bruno.it@gmail.com>
2014-01-20 11:01:00 +0100

    Add support for multiple SSH (deploy) key

--- src/GithubHooks/Hooks/PullOriginHook.php
+++ src/GithubHooks/Hooks/PullOriginHook.php
@@ -16,11 +16,18 @@ class PullOriginHooks implements HookInterface
     protected $path;
 
     /**
-     * @param string $path
+     * @var string
      */
-    public function __construct($path)
+    protected $pathToSshKey;
+
+    /**
+     * @param string $path Path to working copy
+     * @param string $pathToSshKey Path to private SSH key
+     */
+    public function __construct($path, $pathToSshKey = null)
     {
         $this->path = $path;
+        $this->pathToSshKey = $pathToSshKey;
     }
 
     /**
@@ -30,6 +37,15 @@ class PullOriginHooks implements HookInterface
     protected function isPathWritable($path)
     {
         return is_dir($path) && is_writable($path);
+    }
+
+    /**
+     * @param string $pathToSshKey
+     * @return bool
+     */
+    protected function isKeyReadable($pathToSshKey)
+    {
+        return is_readable($pathToSshKey) && is_file($pathToSshKey);
     }
 
     /**
@@ -45,9 +61,23 @@ class PullOriginHooks implements HookInterface
         $aref = explode('/', $payload->ref);
         $branch = $aref[count($aref) - 1];
 
+        $cmd = '';
+
+        if ($this->pathToSshKey) {
+            if (!$this->isKeyReadable($this->pathToSshKey)) {
+                throw new \RuntimeException("SSH key {$this->pathToSshKey} is not readable");
+            }
+
+            $realKeyPath = realpath($this->pathToSshKey);
+
+            $cmd .= "GIT_SSH='ssh -i {$realKeyPath}' ";
+        }
+
+        $cmd .= "git pull origin {$branch}";
+
         $cwd = getcwd();
         chdir($this->path);
-        exec("git pull origin {$branch}");
+        exec($cmd);
         chdir($cwd);
     }
 }
