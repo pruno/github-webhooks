6cfe1f842e7fc6304d9088851110b798ba9fbd2c
Pruno <luca.bruno.it@gmail.com>
2014-01-20 12:36:45 +0100

    Readme, License and Samples

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-github-hooks
+Github Hooks
 ============
 
-Utility to handle multiple Github web-hooks on the same platform
+Utility to handle multiple Github web-hooks on the same platform.
+
+Uses multiple deploy key on the same host.
+
+Sample
+---------
+
+Find it in the sample directory of this repository.
+
+
+Contribute
+---------
+
+Please do. Fork it and send pull requests.
+
+
+License
+---------
+
+This software is released under the New-BSD License.
+A copy can be found in LICENSE.md
--- sample/index.php
+++ sample/index.php
@@ -7,9 +7,13 @@ use \GithubHooks\Hooks\PullOriginHooks;
 include '../vendor/autoload.php';
 
 $repository = new Repository('OwnerName', 'ProjectName');
-$repository->addHook(array('master'), new PullOriginHooks('/path/to/working/copy'));
+$repository->addHook(array('master', 'develop'), new PullOriginHooks('/path/to/working/copy'));
+
+$privateRepository = new Repository('OwnerName', 'ProjectName2');
+$privateRepository->addHook('develop', new PullOriginHooks('/path/to/working/copy2', '/path/to/deploy/key'));
 
 $server = new Server();
-$server->addRespitory('ProjectName', $repository);
+$server->addRepository('ProjectName', $repository);
+$server->addRepository('PrivateProjectName', $privateRepository);
 
 $server->resolve();
